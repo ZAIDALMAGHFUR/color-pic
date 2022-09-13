b657d4c3a766f38ccf804140d68caf8c7e2539e6
ZAIDALMAGHFUR <almaghfurzaid@gmail.com>
2022-09-13 13:52:14 +0700

    main

---
+++ script.js
@@ -0,0 +1,27 @@
+const imgInput = document.querySelector('#img-select')
+const imgPreview = document.querySelector('.preview')
+if (!window.EyeDropper) {
+    alert('Your Browser does not support This is Fail')
+}
+const eyeDropper = new EyeDropper()
+const pickBtn = document.querySelector('.open-picker')
+const result = document.querySelector('.res')
+imgInput.addEventListener('change', function () {
+    const file = this.files[0]
+    if (!file) return
+    const reader = new FileReader()
+    reader.addEventListener('load', function () {
+        imgPreview.src = this.result
+    })
+    reader.readAsDataURL(file)
+})
+pickBtn.addEventListener('click', function () {
+    eyeDropper.open()
+        .then(res => {
+            result.innerHTML = `picked color: <b>${res.sRGBHex}</b>`
+        })
+
+        .catch(err => {
+            console.log("user caancled the slection");
+        })
+})
